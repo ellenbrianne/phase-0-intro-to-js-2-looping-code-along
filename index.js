222963d67e3a2dded2ef09d80b28f863495e2709
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2022-12-24 20:06:15 -0700

    commit tree 145d71d1f7acd05db24b374ab1e29ba57ab10f90

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const thankYouMessage = [];
+
+function writeCards(name, event) {
+  for (let i = 0; i < name.length; i++) {
+    let message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+
+    thankYouMessage.push(message);
+  };
+
+  return thankYouMessage;
+};
+
+let count = 10
+
+function countDown(count) {
+    while (count >= 0) {
+    console.log(count--);
+    };
+};
+
+console.log(countDown(count));
