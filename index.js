d76911b4f16479cc465ed793c562d04e0df6f141
Ellen Bennett <bennette831@gmail.com>
2023-01-18 22:42:47 -0700

    passing lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+let thankYouMessages =  [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    
+    thankYouMessages.push(message);  
+
+    };          
+            
+    return thankYouMessages;
+ 
+}; 
+
+function countDown (num) {
+    while (num > 0) {
+        console.log(num--);
+    };
+
+    console.log(num);
+};
